dc05a51af0a7bc3d8c4cbfc9580b057dec821a84
Brian Xu <bwxu@mit.edu>
2017-11-16 17:47:15 -0500

    updated test_model to get both validation and test accuracy

--- test_model.py
+++ test_model.py
@@ -21,6 +21,12 @@ def test_model():
     tokenizer = Tokenizer(num_words=var.MAX_NUM_WORDS)
     tokenizer.fit_on_texts(train_sentences)
 
+    # Get the val input via tokenizer and val labels
+    val_labels, val_sentences, val_subjects = get_labels_sentences_subjects(var.VALIDATION_DATA_PATH)
+    val_sequences = tokenizer.texts_to_sequences(val_sentences)
+    x_val = pad_sequences(val_sequences, maxlen=var.MAX_SEQUENCE_LENGTH)
+    y_val = to_categorical(np.asarray([var.LABEL_MAPPING[label] for label in val_labels]))
+    
     # Get the test input via tokenizer and test labels
     test_labels, test_sentences, test_subjects = get_labels_sentences_subjects(var.TEST_DATA_PATH)
     test_sequences = tokenizer.texts_to_sequences(test_sentences)
@@ -31,14 +37,19 @@ def test_model():
     if var.USE_SUBJECTS:
         var.SUBJECT_MAPPING = get_mapping(train_subjects)
         x_test_subjects = np.asarray(get_one_hot_vectors(test_subjects, var.NUM_SUBJECTS, var.SUBJECT_MAPPING))
+        x_val_subjects = np.asarray(get_one_hot_vectors(val_subjects, var.NUM_SUBJECTS, var.SUBJECT_MAPPING))
 
     # Evaluate test accuracy
     if var.USE_SUBJECTS:
-        score = model.evaluate([x_test, x_test_subjects], y_test, batch_size=var.BATCH_SIZE)
+        test_score = model.evaluate([x_test, x_test_subjects], y_test, batch_size=var.BATCH_SIZE)
+        val_score = model.evaluate([x_val, x_val_subjects], y_val, batch_size=var.BATCH_SIZE)
     else:
-        score = model.evaluate(x_test, y_test, batch_size=var.BATCH_SIZE)
+        test_score = model.evaluate(x_test, y_test, batch_size=var.BATCH_SIZE)
+        val_score = model.evalulate(x_val, y_val, batch_size=var.BATCH_SIZE)
+
     print()
-    print("test loss = %0.4f, test acc = %0.4f" % (score[0], score[1]))
+    print("val loss = %0.4f, val acc = %0.4f" % (val_score[0], val_score[1]))
+    print("test loss = %0.4f, test acc = %0.4f" % (test_score[0], test_score[1]))
 
 
 if __name__ == '__main__':
