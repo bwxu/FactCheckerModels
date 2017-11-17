3ecf4cc050586409df5a323e55f50895f00dac03
Brian Xu <bwxu@mit.edu>
2017-11-16 22:00:00 -0500

    updated test_model to do basic statistics after testing models

--- test_model.py
+++ test_model.py
@@ -18,11 +18,20 @@ def test_model():
     model_path = sys.argv[1]
 
     if os.path.isdir(model_path):
-        paths = [os.path.join(model_path, f) for f in os.listdir(model_path) if os.path.isfile(os.path.join(model_path, f))]
+        paths = [os.path.join(model_path, f) for f in os.listdir(model_path) if os.path.isfile(os.path.join(model_path, f)) and f.endswith(".hdf5")]
     elif os.path.isfile(model_path):
         paths = [model_path]
-    
-    for path in paths:
+
+    print()
+    print("List of files to test")
+    print(paths)
+    print()
+    model_results = []
+
+    for i, path in enumerate(paths):
+        print()
+        print("Testing file " + str(i + 1) + " out of " + str(len(paths)))
+
         model = load_model(path)
 
         # Recreate the input tokenizer
@@ -57,10 +66,46 @@ def test_model():
             val_score = model.evalulate(x_val, y_val, batch_size=var.BATCH_SIZE)
 
         print()
-        print()
         print("model = " + str(path))
         print("val loss = %0.4f, val acc = %0.4f" % (val_score[0], val_score[1]))
         print("test loss = %0.4f, test acc = %0.4f" % (test_score[0], test_score[1]))
+        model_results.append((os.path.basename(path), round(val_score[0], 4), round(val_score[1], 4), round(test_score[0], 4), round(test_score[1], 4)))
+
+    average_val_loss = round(sum([info[1] for info in model_results])/len(model_results), 4)
+    average_val_acc = round(sum([info[2] for info in model_results])/len(model_results), 4)
+    average_test_loss = round(sum([info[3] for info in model_results])/len(model_results), 4)
+    average_test_acc = round(sum([info[4] for info in model_results])/len(model_results), 4)
+    
+    best_val = sorted(model_results, key=lambda x: x[2], reverse=True)[:5]
+    best_test = sorted(model_results, key=lambda x: x[4], reverse=True)[:5]
+
+    with open("stats.txt", "w") as f:
+        f.write("GLOVE = " + str(not var.USE_WORD2VEC) + " WORD2VEC = " + str(var.USE_WORD2VEC) + "\n")
+        f.write("SUBJECT = " + str(var.USE_SUBJECTS) + "\n")
+        f.write("\n")
+        f.write("Num models = " + str(len(model_results)) + "\n")
+        f.write("Average val_loss = " + str(average_val_loss) + "\n")
+        f.write("Average val_acc = " + str(average_val_acc) + "\n")
+        f.write("Average test_loss = " + str(average_test_loss) + "\n")
+        f.write("Average test_acc = " + str(average_test_acc) + "\n")
+        f.write("\n")
+        f.write("Top 5 validation accuracies:\n")
+        for model in best_val:
+            f.write("\t" + str(model[2]) + "\t" + str(model[0]) + "\n")
+        f.write("\n")
+        f.write("Top 5 test accuracies:\n")
+        for model in best_test:
+            f.write("\t" + str(model[4]) + "\t" + str(model[0]) + "\n")
+        f.write("\n")
+        f.write("Results for all models:\n")
+        for model in model_results:
+            f.write("\t" + str(model) + "\n")
+
+    print()
+    print("------------------------- OVERALL STATISTICS --------------------------")
+    print()
+    with open("stats.txt", "r") as f:
+        print(f.read())
 
 
 if __name__ == '__main__':
