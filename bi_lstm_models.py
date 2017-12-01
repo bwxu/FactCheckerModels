827a346f848dfe690927c7c7576fb35ea1b33628
Brian Xu <bwxu@mit.edu>
2017-11-30 19:45:13 -0500

    added seperate bilstm model

--- bi_lstm_models.py
+++ bi_lstm_models.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import numpy as np
 import var
 
-def lstm_model(embedding_matrix, num_words):
+def bi_lstm_model(embedding_matrix, num_words):
     # Model Definition
     model = Sequential()
     model.add(Embedding(num_words + 1,
@@ -14,7 +14,7 @@ def lstm_model(embedding_matrix, num_words):
                         weights=[embedding_matrix],
                         input_length=var.MAX_SEQUENCE_LENGTH,
                         trainable=var.TRAIN_EMBEDDINGS))
-    model.add(LSTM(128, return_sequences=True))
+    model.add(Bidirectional(LSTM(128, return_sequences=True)))
     model.add(Flatten())
     model.add(Dropout(rate=var.DROPOUT_PROB))
     model.add(Dense(100))
